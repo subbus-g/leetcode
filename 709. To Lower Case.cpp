d7754d4b3d00597bea891f35a291eed2f69a47cb
subbus <subbareddyg83@gmail.com>
2022-03-10 10:51:35 +0530

    add initial setup

---
+++ 709. To Lower Case.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/to-lower-case/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string toLowerCase(string s)
+    {
+        
+        return s;
+    }
+};
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("0.1-in.txt", "r", stdin);
+    freopen("0.3-out.txt", "w", stdout);
+#endif
+    std::ios::sync_with_stdio(false);
+    Solution sol;
+
+    string s;
+    cin >> s;
+    cout << sol.toLowerCase(s);
+
+    return 0;
+}
