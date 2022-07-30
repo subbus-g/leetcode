cd433733b270d6794948a74621412bb5fcea115a
subbus <subbareddyg83@gmail.com>
2022-07-30 23:55:25 +0530

    add 0189 rotate array

---
+++ 0189. Rotate Array/rotate-array.cpp
@@ -0,0 +1,69 @@
+// https://leetcode.com/problems/rotate-array/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// for cin>>vector
+template <typename T>
+istream &operator>>(istream &in, vector<T> &v)
+{
+
+	T x;
+	char sentinel; // for '['
+	in >> sentinel;
+	while (in >> x)
+	{
+		if (in.peek() == ',') // check if the next character is a comma and ignore it
+			in.ignore();
+		v.push_back(x);
+	}
+	return in;
+}
+
+// for cout<<vector
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &v)
+{
+	out << "[";
+	for_each(begin(v), end(v) - 1, [&](const auto &element)
+			 { out << element << ","; });
+	out << v.back() << "]" << endl;
+	return out;
+}
+
+class Solution
+{
+public:
+	void rotate(vector<int> &nums, int k)
+	{
+		std::rotate(nums.begin(), nums.begin() + k + 1, nums.end());
+	}
+};
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("0-!n.txt", "r", stdin);
+	freopen("0-out.txt", "w", stdout);
+#endif
+	std::ios::sync_with_stdio(false);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		string s;
+		int x;
+		cin >> s >> x;
+
+		istringstream iss(s);
+		vector<int> vec;
+		iss >> vec;
+
+		Solution sol;
+		sol.rotate(vec, x);
+		cout << vec;
+		cout << endl;
+	}
+
+	return 0;
+}
