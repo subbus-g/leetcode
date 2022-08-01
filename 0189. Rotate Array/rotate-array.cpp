94e382ef913e19cc4cf6437cef0ce102cfd6129d
subbus <subbareddyg83@gmail.com>
2022-08-01 22:35:03 +0530

    update 0189 rotate array

--- 0189. Rotate Array/rotate-array.cpp
+++ 0189. Rotate Array/rotate-array.cpp
@@ -34,9 +34,21 @@ ostream &operator<<(ostream &out, const vector<T> &v)
 class Solution
 {
 public:
+	void rotate(vector<int> &nums)
+	{
+		int stash = nums[nums.size() - 1];
+		for (int i = nums.size() - 1; i > 0; i--)
+		{
+			nums[i] = nums[i - 1];
+		}
+		nums[0] = stash;
+	}
 	void rotate(vector<int> &nums, int k)
 	{
-		std::rotate(nums.begin(), nums.begin() + k + 1, nums.end());
+		while (k--)
+		{
+			rotate(nums);
+		}
 	}
 };
 int main()
